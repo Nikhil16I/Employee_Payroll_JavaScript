b0d1c69f0ad8d43a49506323297a18052e534053
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-30 22:47:25 +0530

    Calculating Employee Wage For Part Time/Full Time

--- Employee-payroll-JavaScript/Employee_Payroll.js
+++ Employee-payroll-JavaScript/Employee_Payroll.js
@@ -1,11 +1,24 @@
-//UC-1 Checking Employee is Present Or Absent
+//UC-2 Calculating Employee Wage
 const is_Absent = 0
+const is_Part_Time = 1
+const is_Full_Time = 2
+const full_Time_Hours = 8
+const part_Time_Hours = 4
+const wage_Per_Hour = 20
 
-let empCheck = Math.floor(Math.random() *10)%2
-if (empCheck == is_Absent){
-    console.log("Employee is Absent")
-    return
-}else{
-    console.log("Employee is Present")
+let empHrs = 0
+let empCheck = Math.floor(Math.random()*3)
+switch(empCheck) {
+	case is_Part_Time:
+		empHrs = part_Time_Hours
+		break;
+	case is_Full_Time :
+		empHrs = full_Time_Hours
+		break;
+	default :
+		empHrs = 0
 }
+let empWage = empHrs * wage_Per_Hour;
+console.log("Calculating Employee Wage.")
+console.log("Employee Wage is - " + empWage +" units")
 
