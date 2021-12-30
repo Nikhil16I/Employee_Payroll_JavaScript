97b20e7c475de63fe2a086a10d8b5328e6d785e7
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-30 23:01:25 +0530

    Refactored,Getting Working Hours For Employee

--- Employee-payroll-JavaScript/Employee_Payroll.js
+++ Employee-payroll-JavaScript/Employee_Payroll.js
@@ -1,4 +1,4 @@
-//UC-2 Calculating Employee Wage
+//UC-3 Gettig working Hours for daily working Hours
 const is_Absent = 0
 const is_Part_Time = 1
 const is_Full_Time = 2
@@ -6,19 +6,24 @@ const full_Time_Hours = 8
 const part_Time_Hours = 4
 const wage_Per_Hour = 20
 
-let empHrs = 0
-let empCheck = Math.floor(Math.random()*3)
-switch(empCheck) {
-	case is_Part_Time:
-		empHrs = part_Time_Hours
-		break;
-	case is_Full_Time :
-		empHrs = full_Time_Hours
-		break;
-	default :
-		empHrs = 0
+function get_Working_Emp_Hours(empCheck) {
+	switch(empCheck) {
+		case is_Part_Time:
+			return part_Time_Hours;
+			break;
+		case is_Full_Time :
+			return full_Time_Hours;
+			break;
+		default :
+			return 0
+	}
 }
-let empWage = empHrs * wage_Per_Hour;
+let empHrs=0
+
+let empCheck = Math.floor(Math.random()*10)%3
+empHrs = get_Working_Emp_Hours(empCheck)
+
+let empWage = empHrs * wage_Per_Hour;  
+
 console.log("Calculating Employee Wage.")
 console.log("Employee Wage is - " + empWage +" units")
-
