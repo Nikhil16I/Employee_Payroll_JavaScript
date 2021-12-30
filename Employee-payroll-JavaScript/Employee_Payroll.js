ae7a8b69f54e263ce0c94f70ffe089742eb18979
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-30 23:29:33 +0530

    Calculating wages till Total Working Hours per Month is reached

--- Employee-payroll-JavaScript/Employee_Payroll.js
+++ Employee-payroll-JavaScript/Employee_Payroll.js
@@ -5,7 +5,8 @@ const is_Full_Time = 2
 const full_Time_Hours = 8
 const part_Time_Hours = 4
 const wage_Per_Hour = 20
-const num_Of_Working_Days=2
+const num_Of_Working_Days=24
+const max_Hrs_In_Month =120
 
 function get_Working_Emp_Hours(empCheck) {
 	switch(empCheck) {
@@ -21,12 +22,17 @@ function get_Working_Emp_Hours(empCheck) {
 }
 let empHrs=0
 
-for (let day = 0; day < num_Of_Working_Days; day++) {
+let total_Emp_Hrs=0
+let toal_Working_Days=0
+ 
+while (total_Emp_Hrs <= max_Hrs_In_Month && toal_Working_Days < num_Of_Working_Days) {
+  toal_Working_Days++ 
 let empCheck = Math.floor(Math.random()*10)%3
-empHrs = get_Working_Emp_Hours(empCheck)
+total_Emp_Hrs += get_Working_Emp_Hours(empCheck)
 }
-let empWage = empHrs * wage_Per_Hour
+let empWage = total_Emp_Hrs * wage_Per_Hour
 
 console.log("Calculating Employee Wage.")
 console.log("Employee Wage is - " + empWage +" units")
-console.log(" & Total Hours - " + empHrs +"Hours")
+console.log(" & Total Hours - " + total_Emp_Hrs +"Hours")
+console.log("In Total Working Days As -"+toal_Working_Days+" Days")
